e44bab96c0e123177cc5b9938c465370dabbaee1
donavonelli <donavonelli@yahoo.com>
2020-11-20 18:52:15 -0500

    updated company routes to include review routes

--- routes/company.js
+++ routes/company.js
@@ -7,6 +7,11 @@ router.get('/:id', controller.company.show);
 router.post('/', controller.company.create);
 router.put('/:id', controller.company.update);
 router.delete('/:id', controller.company.destroy);
-
+// Review routes
+router.get('/:id/reviews', controller.company.reviewIndex);
+router.get('/:id/reviews/:reviewId', controller.company.showReview);
+router.post('/:id/reviews', controller.company.createReview);
+router.put('/:id/reviews/:reviewId', controller.company.updateReview);
+router.delete('/:id/reviews/:reviewId', controller.company.deleteReview);
 // exports
 module.exports = router;
