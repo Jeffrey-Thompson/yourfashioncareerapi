85c7864816fa022ab171933642a1ecc19c5cace0
donavonelli <donavonelli@yahoo.com>
2020-11-23 12:04:39 -0500

    fixed bug in routes for company

--- routes/company.js
+++ routes/company.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const controller = require('../controllers');
 
 // routes
-router.get('/', controller.company.index);
-router.get('/:id', controller.company.show);
-router.post('/', controller.company.create);
-router.put('/:id', controller.company.update);
-router.delete('/:id', controller.company.destroy);
+router.get('/', controller.companies.index);
+router.get('/:id', controller.companies.show);
+router.post('/', controller.companies.create);
+router.put('/:id', controller.companies.update);
+router.delete('/:id', controller.companies.destroy);
 // Review routes
-router.get('/:id/reviews', controller.company.reviewIndex);
-router.get('/:id/reviews/:reviewId', controller.company.showReview);
-router.post('/:id/reviews', controller.company.createReview);
-router.put('/:id/reviews/:reviewId', controller.company.updateReview);
-router.delete('/:id/reviews/:reviewId', controller.company.deleteReview);
+router.get('/:id/reviews', controller.companies.reviewIndex);
+router.get('/:id/reviews/:reviewId', controller.companies.showReview);
+router.post('/:id/reviews', controller.companies.createReview);
+router.put('/:id/reviews/:reviewId', controller.companies.updateReview);
+router.delete('/:id/reviews/:reviewId', controller.companies.deleteReview);
 // exports
 module.exports = router;
