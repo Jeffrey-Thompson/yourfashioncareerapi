970e7c5cec0e77e12ed793d13ff77b3e90433c5c
JeffreyThompson <jeffathomp@gmail.com>
2021-01-03 14:16:50 -0600

    reviews will update company rating

--- controllers/company.js
+++ controllers/company.js
@@ -55,8 +55,6 @@ const reviewIndex = (req, res) => {
 
         if(!foundReviews) return res.status(200).json({ "message": "No Company with that id found in db" });
 
-        // if(!foundCompany.reviews) return res.status(200).json({ "message": "No reviews for this company in db" });
-
         res.status(200).json({ "reviews": foundReviews });
     });
 };
@@ -83,6 +81,42 @@ const createReview = (req, res) => {
     db.Review.create(req.body, (err, savedReview) => {
         if (err) console.log('Error in Review#create:', err);
 
+        db.Company.findById(req.body.company, (err, company) => {
+            if (err) console.log('Error in finding company to update:', err);
+
+            const authenticity = (req.body.authenticity+(company.authenticity*company.numReviews))/(company.numReviews + 1);
+            const development = (req.body.development+(company.development*company.numReviews))/(company.numReviews + 1);
+            const diversity = (req.body.diversity+(company.diversity*company.numReviews))/(company.numReviews + 1);
+            const freedom = (req.body.freedom+(company.freedom*company.numReviews))/(company.numReviews + 1);
+            const growth = (req.body.growth+(company.growth*company.numReviews))/(company.numReviews + 1);
+            const management = (req.body.management+(company.management*company.numReviews))/(company.numReviews + 1);
+            const salary = (req.body.salary+(company.salary*company.numReviews))/(company.numReviews + 1);
+            const sustainability = (req.body.sustainability+(company.sustainability*company.numReviews))/(company.numReviews + 1);
+            const treatment = (req.body.treatment+(company.treatment*company.numReviews))/(company.numReviews + 1);
+            const workLife = (req.body.workLife+(company.workLife*company.numReviews))/(company.numReviews + 1);
+            const overall = (req.body.overall+(company.overall*company.numReviews))/(company.numReviews + 1);
+            const numReviews = company.numReviews + 1;
+            const companyUpdate = {
+                authenticity,
+                development,
+                diversity,
+                freedom,
+                growth,
+                management,
+                salary,
+                sustainability,
+                treatment,
+                workLife,
+                overall,
+                numReviews
+            }
+
+            db.Company.findByIdAndUpdate(req.body.company, companyUpdate, {new: true}, (err, updatedCompany) => {
+                if (err) console.log('Error in updating company:', err);
+
+                console.log('company updated with review: ', updatedCompany);
+            });
+        })
         res.status(201).json({ "review": savedReview });
     });
 };
